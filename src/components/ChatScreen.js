7acfddedc3d906b6744ec554f442f5f1f3930a25
unknown <nbatre1@yahoo.com>
2019-01-23 00:59:43 -0600

    keep moving forward

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -22,7 +22,7 @@ class ChatScreen  extends Component {
 
         chatManager
          .connect()
-             .then(current =>{
+             .then(currentUser =>{
                  this.setState({currentUser})
              })
              .catch(error => console.error('error',error));
@@ -30,11 +30,47 @@ class ChatScreen  extends Component {
     }
 
     render(){
-        return(
-            <div>
-             <h1>Chat</h1>
+       
+         const styles ={
+             container:{
+                 height:'100vh',
+                 display:'flex',
+                 flexDirection:'column'
+             },
+
+             chatContainer:{
+                 display:'flex',
+                 flex:1
+             },
+
+             whosOnlineListContainer: {
+                width: '300px',
+                flex:'none',
+                padding:20,
+                backgroundColor:'#006994',
+                color:'lightGreen'
+             },
+               chatListContainer:{
+                   padding:20,
+                   width:'85%',
+                   display:'flex',
+                   flexDirection:'column'
+               },
+
+         }
+
+         return(
+             <div style ={styles.container}>
+              <div style={styles.chatContainer}>
+                <aside style ={styles.whosOnlineListContainer}>
+                   <h2>If you are online say hi</h2>
+                </aside>
+                <section style ={styles.chatListContainer}>
+                  <h2>Do you like the layout</h2>
+                </section>
+              </div>
              </div>
-        )
+         )
         
     }
 }
